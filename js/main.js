71239b8c4eba0478830f9baa9e811b15553ceb16
fulmack <sylvainvantieghem@gmail.com>
2019-04-11 09:40:02 +0200

    in index.html change & delete & replace tag

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
-document.getElementsByClassName("score1")[1].innerHTML = 0;
-document.getElementsByClassName("score2")[1].innerHTML = 0;
+document.getElementsByClassName("score1").innerHTML = 0;
+document.getElementsByClassName("score2").innerHTML = 0;
 let maxiScore =5;
 
 
